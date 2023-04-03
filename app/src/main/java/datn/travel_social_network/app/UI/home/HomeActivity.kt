1a48fffe6e1f9c3abce213dd6540243e5a28a867
ptdung <phamtiendung652@gmail.com>
2023-04-03 21:48:31 +0700

    first commit

---
+++ app/src/main/java/datn/travel_social_network/app/UI/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package datn.travel_social_network.app.UI.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import datn.travel_social_network.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/datn/travel_social_network/app/UI/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package datn.travel_social_network.app.UI.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import datn.travel_social_network.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
