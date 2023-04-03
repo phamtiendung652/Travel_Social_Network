2409c012c2eb924c440c1c356921fee62b31f814
ptdung <phamtiendung652@gmail.com>
2023-04-03 21:28:25 +0700

    upload project

---
+++ app/src/main/java/datn/travel_social_network/app/MainActivity.kt
@@ -0,0 +1,12 @@
+package datn.travel_social_network.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import datn.travel_social_network.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
