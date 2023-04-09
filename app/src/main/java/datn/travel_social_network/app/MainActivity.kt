54e776cc86d79ce39f4f4f6301f8be19ead09d33
ptdung <phamtiendung652@gmail.com>
2023-04-09 21:58:13 +0700

    Ui design register

--- app/src/main/java/datn/travel_social_network/app/MainActivity.kt
+++ app/src/main/java/datn/travel_social_network/app/MainActivity.kt
@@ -3,10 +3,16 @@ package datn.travel_social_network.app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import datn.travel_social_network.R
+import datn.travel_social_network.app.UI.login.LoginFragment
+import datn.travel_social_network.app.UI.register.RegisterFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val frmUI = LoginFragment()
+
+        supportFragmentManager.beginTransaction().replace(R.id.frm,frmUI).commit()
     }
 }
