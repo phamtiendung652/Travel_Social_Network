72cbbac4c0fde249c3cf8c90a5f189136bf84346
ptdung <phamtiendung652@gmail.com>
2023-04-27 20:59:58 +0700

    Ui Post

--- app/src/main/java/datn/travel_social_network/app/UI/home/HomeActivity.kt
+++ app/src/main/java/datn/travel_social_network/app/UI/home/HomeActivity.kt
@@ -2,7 +2,6 @@ package datn.travel_social_network.app.UI.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import datn.travel_social_network.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
